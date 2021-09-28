586d9c835d1cd6e00c499ed3240ca537f0e0db28
valentin0897 <valentin08@mail.ru>
2021-09-28 20:08:58 +0700

    added flask instance folder

--- app/run.py
+++ app/run.py
@@ -13,8 +13,9 @@ def register_extensions(app):
 
 
 def create_app(config):
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config)
+    app.config.from_pyfile('config.py')
 
     register_extensions(app)
     return app
