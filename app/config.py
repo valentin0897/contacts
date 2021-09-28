9ef3ed9a8c82da5f7824fc5b5880f120cb6797ae
valentin0897 <valentin08@mail.ru>
2021-09-28 18:21:57 +0700

    update config file

--- app/config.py
+++ app/config.py
@@ -1,9 +1,13 @@
-username = "valentin"
+dialect_driver = "postgresql+psycopg2"
+username = "username"
 password = "1234"
 db_name = "contacts"
+host = "localhost"
+port = "5432"
+secret_key = "1234"
 
 class Config():
     DEBUG = True
-    SECRET_KEY = 'S65p4oeVQRRY8n'
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{username}:{password}@localhost/{db_name}" 
+    SECRET_KEY = secret_key
+    SQLALCHEMY_DATABASE_URI = f"{dialect_driver}://{username}:{password}@{host}:{port}/{db_name}" 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
