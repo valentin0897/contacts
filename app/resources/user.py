b61e8623a52146ac32950c8396ea2e41cadb6d5d
valentin0897 <valentin08@mail.ru>
2021-10-11 16:53:36 +0700

    Refactor user: turn messages into const and refactor methods

--- app/resources/user.py
+++ app/resources/user.py
@@ -1,66 +1,92 @@
 from flask_restful import Resource
 from flask import request
+from marshmallow.exceptions import ValidationError
+from app.resources.email import EMAIL_NOT_FOUND
 from schemas.user import UserSchema
 from schemas.phone import PhoneSchema
 from schemas.email import EmailSchema
 from models.user import UserModel
 from models.phone import PhoneModel
 from models.email import EmailModel
-import validators
 
 user_schema = UserSchema(partial=True)
 user_list_schema = UserSchema(many=True)
 phone_schema = PhoneSchema(partial=True)
-email_shcema = EmailSchema(partial=True)
+email_schema = EmailSchema(partial=True)
+
+USER_NOT_FOUND = "User not found"
+USER_DELETED = "User deleted"
+USER_ERROR_INSERTING = "An error occured while inserting the user"
+USER_ERROR_UPDATING = "An error occured while updating the user"
+
 
 class User(Resource):
     def post(self):
-        requested_data = request.get_json()
-        user = UserModel.get_user_by_id(requested_data['id'])
-        return user_schema.dump(user), 200 
+        id_ = request.get_json()['id']
+        errors = user_schema.validate({"id": id_})
+        if errors:
+            return {"message": USER_NOT_FOUND, "errors": errors}
+        else:
+            user = UserModel.get_user_by_id(id_)
+            return user_schema.dump(user), 200 
 
 
     def put(self):
         requested_data = request.get_json()
         phones = requested_data.pop("phones")
         emails = requested_data.pop("emails")
-        user = user_schema.load(requested_data)
-
-        if(user):
-            user.save()
-            if (phones):
-                for number in phones:
+        try:
+            user = user_schema.load(requested_data)
+        except ValidationError as errors:
+            return {"message": USER_ERROR_INSERTING, "errors": errors.messages}, 400
+        
+        user.save()
+        if (phones):
+            for number in phones:
+                try:
                     phone = phone_schema.load({"user_id": user.id, "number": number, "category": True})
-                    phone.save()
-            
-            if (emails):
-                for email_name in emails:
-                    email = phone_schema.load({"user_id": user.id, "email": email_name, "category": True})
-                    email.save()
+                except ValidationError as errors:
+                    user.delete()
+                    return {"message": USER_ERROR_INSERTING, "errors": errors.messages}
 
-            return {"message": "Пользователь добавлен"}, 201
-        else:
-            return {"message": "Пользователь не добавлен"}, 400
+                phone.save()
+        
+        if (emails):
+            for email_name in emails:
+                try:
+                    email = email_schema.load({"user_id": user.id, "email": email_name, "category": True})
+                except ValidationError as errors:
+                    user.delete()
+                    return {"message": USER_ERROR_INSERTING, "errors": errors.messages}
+
+                email.save()
+
+        return user_schema.dump(user), 201
 
     def patch(self):
         requested_data = request.get_json()
+        errors = user_schema.validate(requested_data)
+        if errors:
+            return {"message": USER_ERROR_UPDATING, "errors": errors}, 400
+
         user = UserModel.get_user_by_id(requested_data["id"])
         user.fio = requested_data["fio"]
         user.avatar_path = requested_data["avatar_path"]
         user.sex = requested_data["sex"]
         user.birthday = requested_data["birthday"]
         user.address = requested_data["address"]
-        if(user):
-            user.save()
-            return {"message": "Пользователь обновлен"}, 201
-        else:
-            return {"message": "Пользователь не обновлен"}, 201
+        user.save()
+        return user_schema.dump(user), 202
 
     def delete(self):
         requested_data = request.get_json()
         user = UserModel.get_user_by_id(requested_data['id'])
-        user.delete()
-        return {"message": "Пользователь удален"}, 201
+        if user:
+            user.delete()
+            return {"message": USER_DELETED}, 201
+        else:
+            return {"message": EMAIL_NOT_FOUND}, 404
+
 
 class UserList(Resource):
     def post(self):
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,3 +1,6 @@
+from marshmallow.decorators import validates
+from marshmallow.exceptions import ValidationError
+from marshmallow import validate
 from extensions import ma
 from models.user import UserModel
 
@@ -6,9 +9,10 @@ class UserSchema(ma.SQLAlchemySchema):
         model = UserModel
         load_instance = True
     
-    id = ma.auto_field()
-    fio = ma.auto_field()
-    avatar_path = ma.auto_field()
-    sex = ma.auto_field()
+    id = ma.auto_field(validate=validate.Range(min=1))
+    fio = ma.auto_field(validate=validate.Length(min=5, max=64))
+    avatar_path = ma.auto_field(validate=validate.Regexp(r".{1,64}(\.jpg|\.png)"))
+    sex = ma.auto_field(validate=validate.ContainsOnly(["М", "Ж"]))
     birthday = ma.auto_field()
     address = ma.auto_field()
+
